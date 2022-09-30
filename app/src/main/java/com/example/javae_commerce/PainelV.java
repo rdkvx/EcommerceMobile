79e75c0c8a52607af7964b790b49a026edcbcabb
Ana Beatriz <ana.mello@outlook.com>
2022-09-30 20:37:24 -0300

    Novas chamadas de tela do painelv

--- app/src/main/java/com/example/javae_commerce/PainelV.java
+++ app/src/main/java/com/example/javae_commerce/PainelV.java
@@ -2,13 +2,67 @@ package com.example.javae_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PainelV extends AppCompatActivity {
+
+    private Button btnCadProd, btnChecarProduto, btnVendasEfetuadas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painelv);
+
+        btnChecarProduto = (Button) findViewById(R.id.btnChecarProduto);
+        btnCadProd = (Button) findViewById(R.id.btnCadProd);
+        btnVendasEfetuadas = (Button) findViewById(R.id.btnVendasEfetuadas);
+
+
+
+
+        btnCadProd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnCadProdActivity();
+            }
+        });
+
+
+        btnChecarProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnChecarProdutoActivity();
+            }
+        });
+
+        btnVendasEfetuadas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnVendasEfetuadasActivity();
+
+            }
+        });
+
+    }
+
+    private void btnVendasEfetuadasActivity() {
+
+        startActivity(new Intent(PainelV.this, VendEfet.class));
+    }
+
+    private void btnChecarProdutoActivity() {
+
+        startActivity(new Intent(PainelV.this, Envios.class));
+    }
+
+    private void btnCadProdActivity() {
+
+        startActivity(new Intent(PainelV.this, Produtos.class));
     }
 }
--- app/src/main/java/com/example/javae_commerce/Produtos.java
+++ app/src/main/java/com/example/javae_commerce/Produtos.java
@@ -2,13 +2,65 @@ package com.example.javae_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Produtos extends AppCompatActivity {
+
+
+    private Button btnMenuNovoProduto, btnMenuRemoveProduto, btnMenuPdtsEnvioPendente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produtos);
+
+
+        btnMenuNovoProduto = (Button) findViewById(R.id.btnMenuNovoProduto);
+        btnMenuRemoveProduto = (Button) findViewById(R.id.btnMenuRemoveProduto);
+        btnMenuPdtsEnvioPendente = (Button) findViewById(R.id.btnMenuPdtsEnvioPendente);
+
+
+        btnMenuNovoProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnMenuNovoProdutoActivity();
+            }
+        });
+
+        btnMenuRemoveProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnMenuRemoveProdutoActivity();
+            }
+        });
+
+        btnMenuPdtsEnvioPendente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                btnMenuPdtsEnvioPendenteActivity();
+            }
+        });
+
+    }
+
+    private void btnMenuNovoProdutoActivity() {
+
+        startActivity(new Intent(Produtos.this, CadProd.class));
+    }
+
+    private void btnMenuRemoveProdutoActivity() {
+
+        startActivity(new Intent(Produtos.this, RemovProd.class));
+    }
+
+    private void btnMenuPdtsEnvioPendenteActivity() {
+
+        startActivity(new Intent(Produtos.this, Envios.class));
     }
 }
